3037cab82ea2196b2c23a06fa1b87c816f8ee28f
maksimprogrammist <apomaksim@gmail.com>
2021-04-12 09:18:02 +0700

    Дизайн, начальный экран

--- app/src/main/java/com/example/ffff/MainActivity2.kt
+++ app/src/main/java/com/example/ffff/MainActivity2.kt
@@ -1,17 +1,11 @@
 package com.example.ffff
 
-import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import com.example.ffff.R
+import android.os.Bundle
 
-
-class MainActivity : AppCompatActivity() {
-
+class MainActivity2 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-
-
-
+        setContentView(R.layout.activity_main2)
     }
 }
